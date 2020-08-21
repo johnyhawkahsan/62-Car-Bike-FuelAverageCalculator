484ca10b632a4e60bd0e48719875f4139efc90d6
Ahsan <johnyhawkahsan@gmail.com>
2020-08-21 10:27:06 +0500

    Initial Commit- Bismillah

---
+++ app/src/main/java/com/johnyhawkdesigns/a62_car_bike_fuelaveragecalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.johnyhawkdesigns.a62_car_bike_fuelaveragecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

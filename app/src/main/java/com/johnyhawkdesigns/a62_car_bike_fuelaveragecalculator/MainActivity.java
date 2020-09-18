e24f1f3b58dc821115f8c2a33f065b1026def4cd
Ahsan <johnyhawkahsan@gmail.com>
2020-09-18 15:47:44 +0500

    2nd commit- Trying to follow my tailor app and child health app for idea. Also fixed AndroidX dependencies for RecyclerView and ConstraintLayout from layouts xmls.

--- app/src/main/java/com/johnyhawkdesigns/a62_car_bike_fuelaveragecalculator/MainActivity.java
+++ app/src/main/java/com/johnyhawkdesigns/a62_car_bike_fuelaveragecalculator/MainActivity.java
@@ -1,14 +1,19 @@
 package com.johnyhawkdesigns.a62_car_bike_fuelaveragecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerViewVehicle);
     }
 }
